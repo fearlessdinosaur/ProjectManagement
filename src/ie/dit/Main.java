7800474af89106fc3aba5491e91d9be76a89044a
fearlessdinosaur <dcraigfox@hotmail.com>
2017-03-06 12:14:35 +0000

    initial commit

---
+++ src/ie/dit/Main.java
@@ -0,0 +1,11 @@
+package ie.dit;
+
+/**
+ * Created by David on 06/03/2017.
+ */
+public class Main {
+    public static void main(String args[])
+    {
+        System.out.println("hello World");
+    }
+}
