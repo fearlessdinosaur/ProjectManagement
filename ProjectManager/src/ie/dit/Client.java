c9588d7def291ac384eb6589952e27619922c78b
fearlessdinosaur <dcraigfox@hotmail.com>
2017-03-15 22:08:32 +0000

    rearraged files and added data structures

--- ProjectManager/src/ie/dit/Client.java
+++ ProjectManager/src/ie/dit/Client.java
@@ -1,8 +1,42 @@
 package ie.dit;
 
-/**
- * Created by David on 06/03/2017.
- */
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.*;
+
+import java.io.IOException;
+
+import java.util.*;
 public class Client {
+    static List<NameValuePair> NamePairs = new ArrayList<>();
+    String name="jim";
+    String pass = "pass";
+    void post()
+   {
+
+
+       HttpClient client = HttpClientBuilder.create().build();
+       NamePairs.add(new BasicNameValuePair("name",name));
+       NamePairs.add(new BasicNameValuePair("Password",pass));
+
+       HttpPost data = new HttpPost("http://192.168.1.16:1440");
+       try
+       {
+           data.setEntity(new UrlEncodedFormEntity(NamePairs));
+           HttpResponse reply = client.execute(data);
+           System.out.println("success");
+
+       } catch (IOException e)
+       {
+           e.printStackTrace();
+       }
+
+   }
 
 }
--- ProjectManager/src/ie/dit/Main.java
+++ ProjectManager/src/ie/dit/Main.java
@@ -2,31 +2,13 @@ package ie.dit;
 
 /**
  * Created by David on 06/03/2017.
- */import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.IOException;
+ */
 
 public class Main {
-    private static HttpClient client = HttpClientBuilder.create().build();
-    public static void main(String args[]) {
-
-        HttpPost httppost = new HttpPost("http://192.168.1.16:1440");
-        try
-        {
-            client.execute(httppost);
-            System.out.println("success");
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
+    public static void main(String Args[])
+    {
+        Client client = new Client();
+        client.post();
     }
+
 }
